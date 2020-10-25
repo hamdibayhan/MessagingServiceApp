6ed74afec12ebe76c3b54e5f18e846b83ae2f319
Hamdi Bayhan <hamdibayhan@gmail.com>
2020-10-25 03:14:56 +0300

    Generic logger extentions and response were added

---
+++ MessagingServiceApp/Dto/ApiResponse/GenericResponse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MessagingServiceApp.Dto.ApiResponse
+{
+    public class Response<T>
+    {
+        public T Result { get; set; }
+        public string Message { get; set; }
+        public bool Success { get; set; }
+        public Exception Exception { get; set; }
+
+        public static Response<T> GetResponse(T obj, bool success, string message = "", Exception ex = null)
+        {
+            return new Response<T>
+            {
+                Result = obj,
+                Success = success,
+                Message = message,
+                Exception = ex
+            };
+        }
+        public static Response<T> GetSuccess(T obj, string message = null)
+        {
+            return GetResponse(obj, true, message, null);
+        }
+        public static Response<T> GetError(Exception ex = null, string message = null, T obj = default(T))
+        {
+            return GetResponse(obj, false, message, ex);
+        }
+    }
+}
---
+++ MessagingServiceApp/Libraries/LoggerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+public static class LoggerExtensions
+{
+    public static void LogError(this ILogger logger, Exception ex, string message = null, params object[] args)
+    {
+        logger.LogError(default(EventId), ex, message, args);
+    }
+    public static void LogInfo(this ILogger logger, string message = null, params object[] args)
+    {
+        logger.LogInformation(message, args);
+    }
+}
