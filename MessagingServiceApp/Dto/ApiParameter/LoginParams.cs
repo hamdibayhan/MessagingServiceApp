a661ac7a7902093c4c79ad9e3f88a0e2185a01b7
Hamdi Bayhan <hamdibayhan@gmail.com>
2020-10-25 19:52:12 +0300

    Login and Register Api request models were created

---
+++ MessagingServiceApp/Dto/ApiParameter/LoginParams.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MessagingServiceApp.Dto.ApiParameter
+{
+    public class LoginParams
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Please enter a valid email")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ MessagingServiceApp/Dto/ApiParameter/RegisterParams.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MessagingServiceApp.Dto.ApiParameter
+{
+    public class RegisterParams
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Please enter a valid email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
