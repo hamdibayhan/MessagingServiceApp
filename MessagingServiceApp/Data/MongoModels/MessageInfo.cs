957225ec359bf95685ba2a5cf8be0a722c7b6e06
Hamdi Bayhan <hamdibayhan@gmail.com>
2020-10-29 16:59:19 +0300

    Created MessageInfo model for Mongo DB

---
+++ MessagingServiceApp/Data/MongoModels/MessageInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MessagingServiceApp.Data.MongoModel
+{
+    public class MessageInfo
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string SenderUserId { get; set; }
+
+        public string SenderUserEmail { get; set; }
+
+        public string ContactUserId { get; set; }
+
+        public string ContactUserEmail { get; set; }
+
+        public string MessageText { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
