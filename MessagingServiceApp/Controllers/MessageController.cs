f51dd41d42a81c347004ea252d4cf2029d657589
Hamdi Bayhan <hamdibayhan@gmail.com>
2020-11-01 18:30:52 +0300

    User block control was added in MessageController SendMessage endpoint

--- MessagingServiceApp/Controllers/MessageController.cs
+++ MessagingServiceApp/Controllers/MessageController.cs
@@ -19,15 +19,18 @@ namespace MessagingServiceApp.Controllers
     {
         private readonly IMessageService messageService;
         private readonly IUserProviderService userProvider;
+        private readonly IUserService userService;
         private readonly ILogger<MessageController> logger;
 
         public MessageController(
             IMessageService messageService,
             IUserProviderService userProvider,
+            IUserService userService,
             ILogger<MessageController> logger)
         {
             this.messageService = messageService;
             this.userProvider = userProvider;
+            this.userService = userService;
             this.logger = logger;
         }
 
@@ -45,6 +48,10 @@ namespace MessagingServiceApp.Controllers
 
                 if (contactUser == null)
                     return NotFound(Response<string>.GetError(null, $"There is no user with name: '{messageInfo.ContactUserName}'"));
+
+                var isUserBlocked = userService.IsUserAlreadyBlocked(contactUser, senderUser);
+                if (isUserBlocked)
+                    return BadRequest(Response<string>.GetError(null, $"User restricted for sending message to contact user"));
 
                 var result = messageService.CreateMessageInfo(messageInfo, senderUser, contactUser);
                 if (result != null)
